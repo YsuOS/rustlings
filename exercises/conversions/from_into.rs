b848801bbee96d84791a610d152c8b52874046aa
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2021-12-31 13:28:54 +0900

    from_into has done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,16 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut p: Vec<&str> = s.split(",").collect();
+        if p.len() == 2 && p[0].len() > 0 {
+            if let Ok(p_age) = p[1].parse::<usize>() {
+                return Person{ name: p[0].to_string(), age: p_age };
+            }
+        }
+        Person::default()
     }
 }
 
