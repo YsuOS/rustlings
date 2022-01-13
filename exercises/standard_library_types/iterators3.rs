eb579d0f7a280dec231cfb6ac5863d3574a20b71
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2022-01-14 00:59:08 +0900

    update iterators3

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -25,27 +25,24 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
     if b == 0 {
         return Err(DivisionError::DivideByZero);
     }
-    if a % b != 0 {
-        return Err(DivisionError::NotDivisible(NotDivisibleError {dividend: a, divisor: b}))
+    match a % b {
+        0 => Ok(a / b),
+        _ => Err(DivisionError::NotDivisible(NotDivisibleError {dividend: a, divisor: b})),
     }
-    Ok(a / b)
 }
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: Ok([1, 11, 1426, 3])
 fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27)).collect();
-    division_results
+    numbers.into_iter().map(|n| divide(n, 27)).collect()
 }
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
 fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27))
-                                    .collect::<Vec<Result<i32, DivisionError>>>();
-    division_results
+    numbers.into_iter().map(|n| divide(n, 27)).collect::<Vec<Result<i32, DivisionError>>>()
 }
 
 #[cfg(test)]
